14efcea44f15c2d371679696efac7192f6292f04
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-03-28 05:26:30 +0530

    added additional changes

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Sanjay Goswami | Sr. Software Engineer | React Rust Raspberrypi';
-export const SITE_DESCRIPTION = 'Personal website of Sanjay Goswami. showcasing the work ';
+export const SITE_DESCRIPTION = 'Personal Website of Sanjay Goswami';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
